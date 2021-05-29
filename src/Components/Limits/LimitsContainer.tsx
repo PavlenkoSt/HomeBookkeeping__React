5238ac7515e911bee7df2c0bb66ce887609840b3
Stanislav <verbatim8872@gmail.com>
2021-05-29 21:08:48 +0300

    limits container on ts

--- src/Components/Limits/LimitsContainer.tsx
+++ src/Components/Limits/LimitsContainer.tsx
@@ -1,15 +1,31 @@
 import Limits from "./Limits"
 import M from 'materialize-css'
-import { useEffect } from "react"
+import { FC, useEffect } from "react"
 import localStore from "../../localStore/localStore"
 import { connect } from "react-redux"
 import { reset } from "redux-form"
-import { deleteLimit, addNewLimit } from '../../Redux/budgetReducer'
+import { deleteLimit, addNewLimit, LimitType } from '../../Redux/budgetReducer'
 import LimitItemContainer from "./LimitItem/LimitItemContainer"
+import { AppStateType } from "../../Redux/reduxStore"
+import { TransactionType } from "../../Redux/chartReducer"
 
-const LimitsContainer = ({ limits, deleteLimit, addNewLimit, reset, historyTransactions }) => {
-    const limitItems = limits.map(limit => <LimitItemContainer key={limit.id} id={limit.id} category={limit.category} sum={limit.limit} deadline={limit.deadline} deleteLimit={deleteLimit} limits={limits} historyTransactions={historyTransactions} />)
-    const onSubmit = formData => {
+type FormDateValuesType = {
+    category: string
+    limit: string
+    days: string
+}
+
+type LimitsContainerPropsType = {
+    limits: Array<LimitType>
+    historyTransactions: Array<TransactionType>
+    deleteLimit: (id: number) => void
+    addNewLimit: (limit: LimitType) => void
+    reset: (formName: string) => void
+}
+
+const LimitsContainer: FC<LimitsContainerPropsType> = ({ limits, deleteLimit, addNewLimit, reset, historyTransactions }) => {
+    const limitItems = limits.map((limit: LimitType) => <LimitItemContainer key={limit.id} id={limit.id} category={limit.category} sum={limit.limit} deadline={limit.deadline} deleteLimit={deleteLimit} limits={limits} historyTransactions={historyTransactions} />)
+    const onSubmit = (formData: FormDateValuesType) => {
         if(!formData.category || !formData.limit || !formData.days){
             M.toast({html: 'Ошибка! Лимит не может быть пустым!'})
         }else{
@@ -36,7 +52,7 @@ const LimitsContainer = ({ limits, deleteLimit, addNewLimit, reset, historyTrans
     return <Limits limitItems={limitItems} onSubmit={onSubmit} />
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state: AppStateType) => ({
     limits: state.budget.limits,
     historyTransactions: state.bill.historyTransactions
 })
