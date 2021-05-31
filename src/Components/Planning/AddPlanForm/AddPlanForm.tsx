161666a2674ef435d809e5ce4e3939a58f146c0e
Stanislav <verbatim8872@gmail.com>
2021-05-31 15:38:36 +0300

    addPlanForm on ts

--- src/Components/Planning/AddPlanForm/AddPlanForm.tsx
+++ src/Components/Planning/AddPlanForm/AddPlanForm.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import { compose } from 'redux'
-import { Field, reduxForm } from 'redux-form'
+import React, { FC } from 'react'
+import { Field, InjectedFormProps, reduxForm } from 'redux-form'
 import { removeSet, addSet } from '../../../helpers/manageFormClasses'
 import { required } from '../../../utilts/validators'
 import { Input } from '../../Add/AddForm/FieldCreator/FieldCreator'
@@ -8,39 +7,40 @@ import s from './AddPlanForm.module.css'
 import manageFormClasses from '../../../helpers/manageFormClasses.module.css'
 
 
-const AddPlanForm = React.memo(({ handleSubmit, valid }) => {
+const AddPlanForm: FC<InjectedFormProps<{}, {}> & {}> = React.memo(({ handleSubmit, valid }) => {
     const formRef = React.createRef()
 
     const clearInputsClasses = () => {
+        //@ts-ignore
         formRef.current.childNodes[0].childNodes.forEach(label => label.classList.remove(manageFormClasses.set))
     }
 
-    return (
-        <form ref={formRef} onSubmit={handleSubmit} className={s.form}>
+    //@ts-ignore
+    return ( <form ref={formRef} onSubmit={handleSubmit} className={s.form}>
             <div className={s.inputsGroup}>
                 <label className={s.label}> 
                     <span className={s.span}>Задача:</span>
                     <Field 
-                    onFocus={addSet} 
-                    onBlur={removeSet}
-                    name='task' 
-                    component={Input} 
-                    className={s.input}
-                    validate={required} 
-                    autoComplete='off' 
+                        onFocus={addSet} 
+                        onBlur={removeSet}
+                        name='task' 
+                        component={Input} 
+                        className={s.input}
+                        validate={required} 
+                        autoComplete='off' 
                     />
                 </label>
                 <label className={s.label}> 
                     <span className={s.span}>Необходимая сумма:</span>
                     <Field 
-                    onFocus={addSet} 
-                    onBlur={removeSet}
-                    name='sum' 
-                    component={Input} 
-                    type='number'
-                    className={s.input}
-                    validate={required} 
-                    autoComplete='off' 
+                        onFocus={addSet} 
+                        onBlur={removeSet}
+                        name='sum' 
+                        component={Input} 
+                        type='number'
+                        className={s.input}
+                        validate={required} 
+                        autoComplete='off' 
                     />
                 </label>
             </div>
@@ -49,6 +49,4 @@ const AddPlanForm = React.memo(({ handleSubmit, valid }) => {
     )
 })
 
-export default compose(
-    reduxForm({ form: 'add-new-plan' })
-)(AddPlanForm)
+export default reduxForm<{}, {}>({ form: 'add-new-plan' })(AddPlanForm)
